2d6e33989dba40d41537f1783c64dcd4fb9e146a
jhon <jfinfocesde@gmail.com>
2025-07-24 20:47:27 -0500

    e10

--- ejercicios-for/ejercicio_for_10.js
+++ ejercicios-for/ejercicio_for_10.js
@@ -1,3 +1,1 @@
-// Ejercicio For 10: Validador de Palíndromos
-// Implementa un programa que use un ciclo for para verificar si una palabra o frase es un palíndromo. 
-// Debe ignorar espacios, mayúsculas y signos de puntuación.
+console.log("Solucion");
